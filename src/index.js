9490d78c390b2c486f9dd8cd9958409fcfc9072c
al4R <alfath.r@mail.ugm.ac.id>
2019-02-12 21:27:06 +0700

    evolusi

--- src/index.js
+++ src/index.js
@@ -11,18 +11,35 @@ import * as serviceWorker from './serviceWorker';
 //   );
 // const element = <h1>Hello, world</h1>;
 // ReactDOM.render(element, document.getElementById('root'));
-function tick() {
-    const element = (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      </div>
-    );
-    ReactDOM.render(element, document.getElementById('root'));
-  }
+// function tick() {
+//     const element = (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+//     ReactDOM.render(element, document.getElementById('root'));
+//   }
   
-  setInterval(tick, 1000);
-//  ReactDOM.render(<App />, document.getElementById('root'));
+//   setInterval(tick, 1000);
+// function Product(props){
+//       return (<div><h1>{props.judul}</h1><h3>harga : {props.harga}</h3></div>);
+//   }
+  
+//   const element = <Product judul="Buku Biru Buku Biru"
+//   harga = "100000"> </Product>;
+//   ReactDOM.render(element, document.getElementById('root'));
+// function Welcome(props) {
+//   return <h1>Holla, {props.name}</h1>;
+// }
+
+// const element = <Welcome name="Cutek" />;
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
+
+//   ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
