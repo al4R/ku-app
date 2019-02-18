141b2507275358465a4ce5b41e30cd0ddce31dbb
al4R <alfath.r@mail.ugm.ac.id>
2019-02-18 21:14:55 +0700

    senin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Product from './Product';
+import Promo from './Promo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="promo"><Promo ></Promo></div>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            holla
-            
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <div className="ap">
+        <Product judul="Koinku " harga="10.000"></Product>
+        </div>
+        <div className="ap">
+        <Product judul="Koinku " harga="10.000"></Product>
+        </div>
+        <div className="ap">
+        <Product judul="Koinku " harga="10.000"></Product>
+        </div>
+        <div className="ap">
+        <Product judul="Koinku " harga="10.000"></Product>
+        </div>
         </header>
       </div>
     );
